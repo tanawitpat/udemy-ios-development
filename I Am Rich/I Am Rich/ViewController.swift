9cc2c70e8649d10259721627c42d5aec9d5d8d4c
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-05-10 23:58:32 +0700

    Section 2, Lecture 23

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tanawit Pattanaveerangkoon on 10/5/2563 BE.
+//  Copyright © 2563 Tanawit Pattanaveerangkoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Udemy - iOS 13 & Swift 5 - The Complete iOS App Development Bootcamp
 
 Course URL: <https://www.udemy.com/course/ios-13-app-development-bootcamp>
+
+## Table of contents
+
+| Project   | Topic                                   |
+| --------- | --------------------------------------- |
+| I Am Rich | Xcode Storyboards and Interface Builder |
